e87f9d43d1c459cb81e1f125875038dbec3ae10e
TheRealFilko <fn.filipnikolic@gmail.com>
2024-02-06 23:42:34 +0100

    Submit solution lasagna-master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,23 +1,46 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
-func PreparationTime()  {
-    
+func PreparationTime(layers []string, prepTimePerLayer int) int {
+    if prepTimePerLayer == 0 {
+        prepTimePerLayer = 2 // Verwende den Standardwert, falls 0 übergeben wird
+    }
+    return len(layers) * prepTimePerLayer
 }
 
 // TODO: define the 'Quantities()' function
-func Quantities()  {
-    
+func Quantities(layers []string) (int, float64){
+    sumNoodles := 0
+    sumSauce := 0.0
+
+    for i := 0; i < len(layers); i++ {
+        if layers[i] == "noodles" {
+            sumNoodles += 50
+        } else if layers[i] == "sauce" {
+            sumSauce += 0.2
+        }
+    }
+    return sumNoodles, sumSauce
 }
 
 // TODO: define the 'AddSecretIngredient()' function
-func AddSecretIngredient()  {
-    
+func AddSecretIngredient(friendsList []string, myList []string) {
+    if len(friendsList) > 0 {
+        // Setze das letzte Element von myList auf das letzte Element von friendsList
+        myList[len(myList)-1] = friendsList[len(friendsList)-1]
+    }
 }
 
 // TODO: define the 'ScaleRecipe()' function
-func ScaleRecipe()  {
-    
+func ScaleRecipe(quantitiesForTwo []float64, portions int) []float64 {
+    scaleFactor := float64(portions) / 2
+    scaleQuantities := make([]float64, len(quantitiesForTwo))
+
+    for i, quantity := range quantitiesForTwo{
+        scaleQuantities[i] = quantity * scaleFactor
+    }
+
+    return scaleQuantities
 }
 
 // Your first steps could be to read through the tasks, and create
