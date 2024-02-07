e50660ce9cc3cdc688ada0817233fd99562a27e5
TheRealFilko <fn.filipnikolic@gmail.com>
2024-02-07 16:29:38 +0100

    Finished solution

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -2,25 +2,56 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+    foo := make(map[string]int)
+    foo["quarter_of_a_dozen"] = 3
+    foo["half_of_a_dozen"] = 6
+    foo["dozen"] = 12
+    foo["small_gross"] = 120
+    foo["gross"] = 144
+    foo["great_gross"] = 1728
+    return foo
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+    bar := make(map[string]int)
+    return bar
 }
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the AddItem() function")
+    if amount, ok := units[unit]; ok {
+        bill[item] += amount
+        return true
+    } else {
+        return false
+    }
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+    if _, ok := bill[item]; !ok {
+        return false
+    }
+
+    if amount, ok := units[unit]; ok {
+        if bill[item] - amount < 0 {
+            return false
+        } else if bill[item] - amount == 0 {
+            delete(bill, item)
+        } else {
+            bill[item] -= amount
+        }
+        return true
+    } else {
+        return false
+    }
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+    if _, ok := bill[item]; ok {
+        return bill[item], true 
+    }
+    return 0, false
 }
