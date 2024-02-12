7ebc512b41b6c1bc938dc44b8ed8f3c20e774701
TheRealFilko <fn.filipnikolic@gmail.com>
2024-02-12 23:34:28 +0100

    Working on the solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,15 @@
 package raindrops
 
+import "fmt"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+    foo := map[int]string{ 3: "Pling", 5: "Plang", 7: "Plong"}
+    for i := 0; i <= 100; i++ {
+        for teiler, klang := range foo {
+            fmt.Println("Zahl: %d", i)
+            if i % teiler == 0 {
+                fmt.Println("Zahl: %s", klang)
+            }
+        }
+    }
 }
