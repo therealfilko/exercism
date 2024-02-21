e7032bc877c177d059f64fb652e9292da2a7002a
TheRealFilko <fn.filipnikolic@gmail.com>
2024-02-21 23:26:34 +0100

    Working on the ll

--- go/simple-linked-list/simple_linked_list.go
+++ go/simple-linked-list/simple_linked_list.go
@@ -1,27 +1,69 @@
 package linkedlist
 
+import "errors"
+
 // Define the List and Element types here.
+type List struct {
+    Head *Node
+}
+
+type Node struct {
+    Value int
+    Next *Node
+}
 
 func New(elements []int) *List {
-	panic("Please implement the New function")
+    list := &List{}
+    for i := len(elements) - 1; i >= 0; i-- {
+        newNode := &Node{Value: elements[i], Next: list.Head}
+        list.Head = newNode
+    }
+    return list
 }
 
 func (l *List) Size() int {
-	panic("Please implement the Size function")
+    sum := 0
+    cur := l.Head
+    for cur != nil {
+        sum++
+        cur = cur.Next
+    }
+    return sum
 }
 
 func (l *List) Push(element int) {
-	panic("Please implement the Push function")
+    newNode := &Node{Value: element, Next: l.Head}
+    l.Head = newNode
 }
 
 func (l *List) Pop() (int, error) {
-	panic("Please implement the Pop function")
+    if l.Head == nil {
+        return -1, errors.New("Empty List")
+    } 
+    cur := l.Head.Value
+    l.Head = l.Head.Next
+
+    return cur, nil
 }
 
 func (l *List) Array() []int {
-	panic("Please implement the Array function")
+    var arr []int
+    cur := l.Head
+    for cur != nil {
+        arr = append(arr, cur.Value)
+        cur = cur.Next
+    }
+    return arr
 }
 
 func (l *List) Reverse() *List {
-	panic("Please implement the Reverse function")
+    cur := l.Head
+    var prev *Node = nil
+    for cur != nil {
+        next := cur.Next
+        cur.Next = prev
+        prev = cur
+        cur = next
+    }
+    return l
 }
