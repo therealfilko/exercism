ca56452a526650ccfb0b2fc5d53dbd15bbc673be
TheRealFilko <fn.filipnikolic@gmail.com>
2024-02-24 22:31:57 +0100

    Added solution to the exercise

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,5 +1,17 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+    lo := 0 
+    hi := len(list) - 1
+    for lo <= hi {
+    mid := lo + (hi - lo)/2
+        if list[mid] == key {
+            return mid
+        } else if list[mid] < key {
+            lo = mid + 1
+        } else {
+            hi = mid - 1
+        }
+    }
+    return -1
 }
